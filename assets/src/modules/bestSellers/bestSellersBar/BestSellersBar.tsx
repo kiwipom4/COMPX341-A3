845f73ae42ab4a7231afe839452833cd044b9456
JH419 <JH419@students.waikato.ac.nz>
2022-05-19 10:55:18 +1200

    Commiting with an error in BestSellersBar.tsx

--- assets/src/modules/bestSellers/bestSellersBar/BestSellersBar.tsx
+++ assets/src/modules/bestSellers/bestSellersBar/BestSellersBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 import { LinkContainer } from "react-router-bootstrap";
 import { NavItem } from "react-bootstrap";
 import "../../../common/styles/gallery.css";
